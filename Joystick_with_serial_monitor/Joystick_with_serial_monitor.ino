69c09b06dcc0d2c98c7fc68200cb89e055906e43
isaac-mwondi <60731842+isaac-mwondi@users.noreply.github.com>
2020-02-15 13:39:35 +0300

    Add files via upload

---
+++ Joystick_with_serial_monitor/Joystick_with_serial_monitor.ino
@@ -0,0 +1,37 @@
+/*Using a joystick with the serial monitor
+The joystick is similar to two potentiometers connected together, 
+one for the vertical movement (Y-axis) and other for the horizontal movement (X-axis).
+
+
+*/
+const int xPin = A0;                                               
+const int yPin = A1;
+const int switchPin = 2;
+
+int xValue;
+int yValue;
+int switchValue;
+
+void setup() 
+{
+  Serial.begin(9600);
+  pinMode(switchPin, INPUT_PULLUP);
+}
+
+void loop() 
+{
+  switchValue = digitalRead(switchPin);
+  xValue = analogRead(xPin);
+  yValue = analogRead(yPin);
+  
+  Serial.print("Switch:  ");
+  Serial.print(switchValue);
+  Serial.print("\n");
+  Serial.print("X-axis: ");
+  Serial.print(xValue);
+  Serial.print("\n");
+  Serial.print("Y-axis: ");
+  Serial.print(yValue);
+  Serial.print("\n\n");
+  delay(1000);
+}
---
+++ Moisture_Sensor/Moisture_Sensor.ino
@@ -0,0 +1,35 @@
+/*
+  Moisture Sensor measures soil moisture grace to the changes in
+  electrical conductivity of the earth ( soil resistance increases with drought ). 
+
+
+ 
+*/
+int sensorPin = A0; 
+
+int sensorValue; 
+
+int limit = 300; 
+
+void setup() {
+  
+ Serial.begin(9600);
+ pinMode(13, OUTPUT);
+ 
+}
+
+void loop() {
+
+ sensorValue = analogRead(sensorPin); 
+ Serial.println("Analog Value : ");
+ Serial.println(sensorValue);
+ 
+ if (sensorValue<limit) {
+ digitalWrite(13, HIGH); 
+ }
+ else {
+ digitalWrite(13, LOW); 
+ }
+ 
+ delay(1000); 
+}
